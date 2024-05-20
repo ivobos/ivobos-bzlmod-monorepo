9663228d3e9b187a37a75290ed509acee1be90a7
Ivo Bosticky <ivobos@gmail.com>
2024-05-20 22:29:24 +1000

    failing test

--- projects/calculator/calculator_test.py
+++ projects/calculator/calculator_test.py
@@ -5,7 +5,10 @@ from projects.calculator.calculator import Calculator
 class TestSum(unittest.TestCase):
     def test_sum(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(1, 2), 3)
+        self.assertEqual(calculator.add(1, 2), 4)
+    def test_sum_negatives(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(-4, -11), -15)
 
 
 if __name__ == '__main__':
